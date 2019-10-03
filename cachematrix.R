4729cc442c755626d165a73e5bbc95fe27313677
ludejia <ludejia@gmail.com>
2019-10-02 20:31:50 -0400

    final

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## makeCacheMatrix create an object containing a matrix and a list of four functions
+## cacheSolve could store an inversed matrix in that object
+
 ## Write a short comment describing this function
+
+## it can set the matrix; get the matrix; set an inversed matrix; get the inversed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
@@ -20,6 +25,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+
+## Check if there is an inversed matrix in that list object,if 
+##it exists, retrieve it. If not, inverse the matrix and cache the inversed one. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
